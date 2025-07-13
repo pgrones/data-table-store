d42bb0c404bcc70733c64de19aebc39716db89c0
Leah Grones <pascal.grones@gmx.at>
2025-07-13 16:16:18 +0200

    Fix cell ellipsis

--- src/mantineDataTable/dataDisplay/cell.tsx
+++ src/mantineDataTable/dataDisplay/cell.tsx
@@ -1,24 +1,22 @@
 import { Cell as DataTableCell } from '@lib';
-import { Box, type BoxProps } from '@mantine/core';
+import { Flex, type BoxProps } from '@mantine/core';
 
 export const Cell = DataTableCell.as<BoxProps>(({ cellValue, ...props }) => {
   return (
-    <Box
-      fz="sm"
-      display="flex"
-      h="100%"
-      {...props}
-      style={[
-        props.style,
-        {
-          whiteSpace: 'nowrap',
-          overflow: 'hidden',
-          textOverflow: 'ellipsis',
-          alignItems: 'center'
-        }
-      ]}
-    >
-      {cellValue}
-    </Box>
+    <Flex fz="sm" h="100%" align="center" {...props}>
+      {typeof cellValue === 'string' ? (
+        <span
+          style={{
+            whiteSpace: 'nowrap',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis'
+          }}
+        >
+          {cellValue}
+        </span>
+      ) : (
+        cellValue
+      )}
+    </Flex>
   );
 });
